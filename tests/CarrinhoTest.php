2adb72a73cef97e70bb6c1956a420481ff60fa65
Miqueias Penha <miqueiaspenha@gmail.com>
2025-06-11 17:26:58 -0300

    test: adiciona test para total produtos e compra

--- tests/CarrinhoTest.php
+++ tests/CarrinhoTest.php
@@ -34,4 +34,39 @@ class CarrinhoTest extends TestCase
         $this->assertInstanceOf(Produto::class, $carrinho->getProdutos()[0]);
         $this->assertInstanceOf(Produto::class, $carrinho->getProdutos()[1]);
     }
+
+    public function testSeValoresDeProdutosNoCarrinhoEstaoCorretosConformePassado()
+    {
+        $produto1 = new Produto();
+        $produto1->setName('Produto 1');
+        $produto1->setPrice(19.99);
+        $produto1->setSlug('produto-1');
+
+        $carrinho = new Carrinho();
+        $carrinho->addProduto($produto1);
+
+        $this->assertEquals('Produto 1', $carrinho->getProdutos()[0]->getName());
+        $this->assertEquals(19.99, $carrinho->getProdutos()[0]->getPrice());
+        $this->assertEquals('produto-1', $carrinho->getProdutos()[0]->getSlug());
+    }
+
+    public function testSeTotalDeProdutosEValorDaCompraEstaoCorretos()
+    {
+        $produto1 = new Produto();
+        $produto1->setName('Produto 1');
+        $produto1->setPrice(19.99);
+        $produto1->setSlug('produto-1');
+
+        $produto2 = new Produto();
+        $produto2->setName('Produto 2');
+        $produto2->setPrice(16.55);
+        $produto2->setSlug('produto-2');
+
+        $carrinho = new Carrinho();
+        $carrinho->addProduto($produto1);
+        $carrinho->addProduto($produto2);
+
+        $this->assertEquals(2, $carrinho->getTotalProdutos());
+        $this->assertEquals(36.54, $carrinho->getTotalCompra());
+    }
 }
